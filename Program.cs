89f5606d7d7a40a55629d44252fce5ca08ca6273
CubeIR <75272685+CubeIR@users.noreply.github.com>
2022-11-29 21:08:51 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,25 +16,29 @@ internal class TSMaker
 
         string heightmap_NormalMap_Switch = Console.ReadLine();
         int answer = int.Parse(heightmap_NormalMap_Switch);
+
         if (answer == 0)
-            Console.WriteLine(" ------- Normal jsons will be created ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want jsons to be generated for\n");
+            Console.WriteLine(" ------- Normal jsons will be created ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want to generate jsons for\n");
         else if (answer == 1)
-            Console.WriteLine(" ------- Heightmap jsons will be created ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want jsons to be generated for\n");
+            Console.WriteLine(" ------- Heightmap jsons will be created ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want to generate jsons for\n");
         else
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Input 0 or 1 only, now close the application");
         }
 
-        string _ = Console.ReadLine();
 
+
+        string _ = Console.ReadLine();
 
         string folderPath = _ + @"\JSONS\";
 
         if (folderPath.EndsWith(@"\\")) folderPath.Replace(@"\\", @"\");
+
         Directory.CreateDirectory(folderPath);
 
         string[] image_Directories_Full = Directory.GetFiles(_);
+
         foreach (string listed_image_Directories_Full in image_Directories_Full)
 
         {
@@ -43,15 +47,14 @@ internal class TSMaker
             string json_Fullpath = folderPath + image_Name_Without_Extension + ".texture_set.json";
             Console.WriteLine(json_Fullpath);
 
-
-
-
-            string json_File_Content = " {\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}} ";
-
-            string[] HN = { "normal", "heightmap" };
-            string MER = image_Name_Without_Extension + "_mer";
-            string heightmap = image_Name_Without_Extension + "_heightmap";
-            string normal = image_Name_Without_Extension + "_normal";
+ //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+            string json_File_Content = " {\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}} "; //
+                                                                                //------------------------------------------------------------------------------------------//
+            string[] HN = { "normal", "heightmap" };                           //
+            string MER = image_Name_Without_Extension + "_mer";               //
+            string normal = image_Name_Without_Extension + "_normal";        //
+            string heightmap = image_Name_Without_Extension + "_heightmap"; //
+//-------------------------------------------------------------------------//
 
             json_File_Content = json_File_Content.Replace("X", image_Name_Without_Extension);
             json_File_Content = json_File_Content.Replace("Y", MER);
@@ -76,12 +79,12 @@ internal class TSMaker
     }
 }
 
-
-// Planned: This app can use a lot less lines, I should also work on strings that are displayed to users/have clearer instructions. DONE
-// Planned: Exclude textures if the file name ends with _mer, _normal, _heightmap, this could be a bit tricky because of some special cases/exceptions.
-// Planned: Re-Run the app whenever user types anything wrong, could be a few cases
-// Planned: This app should do anything with the files aren't PNG, JPG, JPEG, TGA, etc... (or all other supported formats)
-// Planned: Update readme.md with a more accurate description of the app, maybe a little "How To" too in case anyone has any problems.
-// Planned: Reading subdirectories and placing the jsons in the directories with the same folder name inside of JSONS folder.
+// Planned:
+// Use TryParse instead and do something about users typing strings instead of 0 and 1 at start
+// Exclude textures if the file name ends with _mer, _normal, _heightmap, this could be a bit tricky because of some special cases/exceptions.
+// Re-Run the app whenever user types anything wrong, could be a few cases
+// This app should do anything with the files aren't PNG, JPG, JPEG, TGA, etc... (or all other supported formats)
+// Update readme.md with a more accurate description of the app, maybe a little "How To" too in case anyone has any problems.
+// Reading subdirectories and placing the jsons in the directories with the same folder name inside of JSONS folder.
 // e.g App gets files in /blocks/candles, and places candles jsons in JSONS/candles folder */
-// Planned: Get files and create copies of the them with the same name + _mer/normal/heightmap in the same directory (Optional)
+// Get files and create copies of the them with the same name + _mer/normal/heightmap in the same directory (Optional)
