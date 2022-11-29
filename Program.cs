3d6d8653ddbb7fc50fc69dff688a34da62013d6b
CubeIR <75272685+CubeIR@users.noreply.github.com>
2022-11-29 20:43:36 +0330

    better instructions and text, fewer lines
    Green text, texts are now written and formatted better to be displayed, with clearer instructions.
    -30 lines

--- Program.cs
+++ Program.cs
@@ -8,99 +8,79 @@ internal class TSMaker
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Type 0 or 1\n0 for Heightmap texture sets\n1 for Normal map texture sets");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.BackgroundColor = ConsoleColor.Black;
+
+
+        Console.Write("Input 0 or 1\n0 = Normals\n1 = Heightmaps\nType here: ");
+
         string heightmap_NormalMap_Switch = Console.ReadLine();
         int answer = int.Parse(heightmap_NormalMap_Switch);
-        Console.WriteLine("Selected:");
-
-
-        // Heightmaps ----------------------------------------------- Heightmaps
         if (answer == 0)
-        {
-            Console.WriteLine("Heightmaps - Please type or copy the full directory of where your images are located\nIt is usually the /textures/blocks folder in your resource pack\n");
-            string _ = Console.ReadLine();
-
-            // Create a folder to place the jsons in it (and to direct jsons later on)
-            string folderPath = _ + @"\JSONS\";
-            if (folderPath.EndsWith(@"\\")) folderPath.Replace(@"\\", @"\");
-            Directory.CreateDirectory(folderPath);
-
-
-            string[] image_Directories_Full = Directory.GetFiles(_);
-            foreach (string listed_image_Directories_Full in image_Directories_Full)
-
-            {
-                string image_Name_Without_Extension = Path.GetFileNameWithoutExtension(listed_image_Directories_Full);
-
-                string json_Fullpath = folderPath + image_Name_Without_Extension + ".texture_set.json";                           // Defining the full path for the jsons we are going to make
-                Console.WriteLine(json_Fullpath);
-
-
-                string MER = image_Name_Without_Extension + "_mer";
-                string heightmap = image_Name_Without_Extension + "_heightmap";
-
-
-                string json_File_Content = " {\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"heightmap\":\"Z\"}} ";
-                json_File_Content = json_File_Content.Replace("X", image_Name_Without_Extension);
-                json_File_Content = json_File_Content.Replace("Y", MER);
-                json_File_Content = json_File_Content.Replace("Z", heightmap);
-                File.WriteAllText(json_Fullpath, json_File_Content);
-
-            }
-
-            Console.WriteLine("DONE! Find JSONS folder in your textures directory");
-        }
-
-
-
-        // Normal Maps ----------------------------------------------- Normal Maps
+            Console.WriteLine(" ------- Normal jsons will be created ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want jsons to be generated for\n");
         else if (answer == 1)
-        {
-            Console.WriteLine("Normal maps - Please type or copy the full directory of where your images are located\nIt is usually the /textures/blocks folder in your resource pack\n");
-            string _ = Console.ReadLine();
-
-            string folderPath = _ + @"\JSONS\";
-            if (folderPath.EndsWith(@"\\")) folderPath.Replace(@"\\", @"\");
-            Directory.CreateDirectory(folderPath);
-
-
-            string[] image_Directories_Full = Directory.GetFiles(_);
-            foreach (string listed_image_Directories_Full in image_Directories_Full)
-
-            {
-                string image_Name_Without_Extension = Path.GetFileNameWithoutExtension(listed_image_Directories_Full);
-
-                string json_Fullpath = folderPath + image_Name_Without_Extension + ".texture_set.json";
-                Console.WriteLine(json_Fullpath);
-
-
-                string MER = image_Name_Without_Extension + "_mer";
-                string normal = image_Name_Without_Extension + "_normal";
-
-
-                string json_File_Content = " {\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"normal\":\"Z\"}} ";
-                json_File_Content = json_File_Content.Replace("X", image_Name_Without_Extension);
-                json_File_Content = json_File_Content.Replace("Y", MER);
-                json_File_Content = json_File_Content.Replace("Z", normal);
-                File.WriteAllText(json_Fullpath, json_File_Content);
-
-            }
-
-            Console.WriteLine("DONE! Find JSONS folder in your textures directory");
-        }
-
+            Console.WriteLine(" ------- Heightmap jsons will be created ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want jsons to be generated for\n");
         else
         {
-            Console.WriteLine("Type 0 or 1 Only, 0 for heightmaps, 1 for normals\nRe-Run the program");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Input 0 or 1 only, now close the application");
         }
 
-        Console.ReadLine();
+        string _ = Console.ReadLine();
+
+
+        string folderPath = _ + @"\JSONS\";
+
+        if (folderPath.EndsWith(@"\\")) folderPath.Replace(@"\\", @"\");
+        Directory.CreateDirectory(folderPath);
+
+        string[] image_Directories_Full = Directory.GetFiles(_);
+        foreach (string listed_image_Directories_Full in image_Directories_Full)
+
+        {
+            string image_Name_Without_Extension = Path.GetFileNameWithoutExtension(listed_image_Directories_Full);
+
+            string json_Fullpath = folderPath + image_Name_Without_Extension + ".texture_set.json";
+            Console.WriteLine(json_Fullpath);
+
+
+
+
+            string json_File_Content = " {\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}} ";
+
+            string[] HN = { "normal", "heightmap" };
+            string MER = image_Name_Without_Extension + "_mer";
+            string heightmap = image_Name_Without_Extension + "_heightmap";
+            string normal = image_Name_Without_Extension + "_normal";
+
+            json_File_Content = json_File_Content.Replace("X", image_Name_Without_Extension);
+            json_File_Content = json_File_Content.Replace("Y", MER);
+
+            if (answer == 0)
+            {
+                json_File_Content = json_File_Content.Replace("W", HN[0]);
+                json_File_Content = json_File_Content.Replace("Z", normal);
+            }
+            else if (answer == 1)
+            {
+                json_File_Content = json_File_Content.Replace("W", HN[1]);
+                json_File_Content = json_File_Content.Replace("Z", heightmap);
+            }
+            else Environment.Exit(1);
+
+
+            File.WriteAllText(json_Fullpath, json_File_Content);
+        }
+
+        Console.WriteLine($"\nSUCCESSFUL! Find JSONS folder at:\n{folderPath}"); Console.ReadLine();
     }
 }
-// Planned: This app can use a lot less lines, I should also work on strings that are displayed to users/have clearer instructions.
+
+
+// Planned: This app can use a lot less lines, I should also work on strings that are displayed to users/have clearer instructions. DONE
 // Planned: Exclude textures if the file name ends with _mer, _normal, _heightmap, this could be a bit tricky because of some special cases/exceptions.
 // Planned: Re-Run the app whenever user types anything wrong, could be a few cases
-// Planned: This app should do anything with the files aren't PNG, JPG, JPEG, TGA, etc... (all other supported formats)
+// Planned: This app should do anything with the files aren't PNG, JPG, JPEG, TGA, etc... (or all other supported formats)
 // Planned: Update readme.md with a more accurate description of the app, maybe a little "How To" too in case anyone has any problems.
 // Planned: Reading subdirectories and placing the jsons in the directories with the same folder name inside of JSONS folder.
 // e.g App gets files in /blocks/candles, and places candles jsons in JSONS/candles folder */
