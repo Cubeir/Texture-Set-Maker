0712f1e2b0f2d085637c92e15ca86799da16cdbe
CubeIR <75272685+CubeIR@users.noreply.github.com>
2022-12-14 17:35:54 +0330

    Exclude unsupported file formats
    Exclude unsupported file formats from Texture Set creation process

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using Newtonsoft.Json;
 using System.IO;
 using System.Media;
 using static System.Console;
-
+using Newtonsoft.Json.Linq;
+using System.Runtime.CompilerServices;
 
 internal class TSMaker
 {
@@ -12,7 +13,7 @@ internal class TSMaker
         ForegroundColor = ConsoleColor.Yellow;
         BackgroundColor = ConsoleColor.Black;
 
-
+        // User controls heightmap/normal json creation
         Write("Input 0 or 1\n0 = Normals\n1 = Heightmaps\nType here: ");
 
         string heightmap_NormalMap_Switch = ReadLine();
@@ -32,26 +33,45 @@ internal class TSMaker
         {
             ForegroundColor = ConsoleColor.DarkRed;
             WriteLine("Input a number (0 or 1 only) now close the application");
+        }
+
+
+        // Store all full file directories
+        string _ = ReadLine();
+        string[] image_Directories_Full = Directory.GetFiles(_);
+
+        List<string> image_Directories_Full_List = new List<string>();
+        image_Directories_Full_List = image_Directories_Full.ToList();
+
+
+        // Define the folder path
+        string folderPath = _ + @"\JSONS\";
+        if (folderPath.EndsWith(@"\\"))
+            folderPath.Replace(@"\\", @"\");
+        Directory.CreateDirectory(folderPath);
+
+
+        // Get rid of unsupported formats
+        for (int i = image_Directories_Full_List.Count() - 1; i >= 0; i--)
+        {
+
+            if (!(image_Directories_Full_List[i].EndsWith(".png") || image_Directories_Full_List[i].EndsWith(".jpg") || image_Directories_Full_List[i].EndsWith(".tga") || image_Directories_Full_List[i].EndsWith(".jpeg")))
+            {
+                image_Directories_Full_List.RemoveAt(i);
+            }
 
         }
 
 
-        string _ = ReadLine();
-        string folderPath = _ + @"\JSONS\";
 
-        if (folderPath.EndsWith(@"\\")) { folderPath.Replace(@"\\", @"\"); }
-
-        Directory.CreateDirectory(folderPath);
-
-        string[] image_Directories_Full = Directory.GetFiles(_);
-
-
-        foreach (string listed_image_Directories_Full in image_Directories_Full)
+        // Factory
+        foreach (string listed_image_Directories_Full in image_Directories_Full_List)
 
         {
+
             string image_Name_Without_Extension = Path.GetFileNameWithoutExtension(listed_image_Directories_Full);
 
-
+            // Discard unneeded files
             if (image_Name_Without_Extension.EndsWith("mer")) continue;
             else if (image_Name_Without_Extension.EndsWith("heightmap")) continue;
             else if (image_Name_Without_Extension.EndsWith("texture_set")) continue;
@@ -59,15 +79,15 @@ internal class TSMaker
 
             string json_Fullpath = folderPath + image_Name_Without_Extension + ".texture_set.json";
             WriteLine(json_Fullpath);
-              
-            //------------
+
+            //------------------------------------------- Base information which we use to create the json's content
             string json_File_Content = "{\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}}";
 
             string[] NH = { "normal", "heightmap" };
             string MER = image_Name_Without_Extension + "_mer";
             string normal = image_Name_Without_Extension + "_normal";
             string heightmap = image_Name_Without_Extension + "_heightmap";
-            //-------------
+            //------------------------------------ Customize this if you have named your textures in a different way
 
             json_File_Content = json_File_Content.Replace("X", image_Name_Without_Extension);
             json_File_Content = json_File_Content.Replace("Y", MER);
@@ -84,11 +104,12 @@ internal class TSMaker
             }
             else Environment.Exit(1);
 
-
+           
             File.WriteAllText(json_Fullpath, json_File_Content);
         }
  
-       
+
+        // Finish
         if (answer == 0 || answer == 1)
         {
             WriteLine($"\nSUCCESSFUL! Find JSONS folder at:\n{folderPath}");
@@ -97,20 +118,19 @@ internal class TSMaker
             finishSound.Play();
         }
         ReadLine();
-    }
+    } 
 }
 
 // ideas/plans:
-
-// This app should not do anything with the files that aren't PNG, JPG, JPEG, TGA etc.. all other supported formats.
 // Re-Run the app whenever user types anything wrong, could be a few cases
 // Reading subdirectories and placing the jsons in the directories with the same folder name inside of JSONS folder.
 // e.g get files in /blocks/candles, and places candles jsons in JSONS/candles folders.
-// Get files and create copies of the them with the same name + _mer/normal/heightmap in the same directory (Optional Feature, answer 0 0 == normal map, without file copy, 0 1 == normal map with file copy)|
+// Get files and create copies of the them with the same name + _mer/normal/heightmap in the same directory (Optional Feature)
 // Move it all to an actual Ui
 
 
 // v2 so far:
+// This app should not do anything with the files that aren't PNG, JPG, JPEG, TGA etc.. all other supported formats.
 // General improvements e.g., cleaner text, better exception handling, etc...
 // Exclude textures if the file name ends with _mer, _normal, _heightmap, this could be a bit tricky because of some special cases/exceptions
 // Update readme.md with a more accurate description of the app, maybe a little "How To" too in case anyone has any problems
