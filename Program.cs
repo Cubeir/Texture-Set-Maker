87ccb155f6ae7578be5a1167da4e75f36f2ff7ab
Reza Soltani <75272685+CubeIR@users.noreply.github.com>
2023-01-08 20:15:40 +0330

    Generate everything without asking.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ using static System.Console;
 using Newtonsoft.Json.Linq;
 using System.Runtime.CompilerServices;
 using System.Diagnostics;
+using ImageProcessor.Imaging.Formats;
+using ImageProcessor;
+using static System.Net.WebRequestMethods;
+using System.Drawing;
+using File = System.IO.File;
+using System.Drawing.Imaging;
 
 internal class TSMaker
 {
@@ -13,32 +19,10 @@ internal class TSMaker
     {
         ForegroundColor = ConsoleColor.Yellow;
         BackgroundColor = ConsoleColor.Black;
-
-        // User controls heightmap/normal json creation
-        Write("Input 0 or 1\n0 = Normals\n1 = Heightmaps\nType here: ");
-
-        string heightmap_NormalMap_Switch = ReadLine();
-        if (Int32.TryParse(heightmap_NormalMap_Switch, out int answer))
-        {
-            if (answer == 0)
-                WriteLine(" ------- Let's make some Normal Map jsons ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want to generate jsons for\n");
-            else if (answer == 1)
-                WriteLine(" ------- Let's make some Heightmap jsons ------- \nPlease type or copy the full directory of the folder where your images are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\nIsolate the files that you want to generate jsons for\n");
-            else
-            {
-                ForegroundColor = ConsoleColor.Red;
-                WriteLine("Input 0 or 1 only, now close the application");
-            }
-        }
-        else
-        {
-            ForegroundColor = ConsoleColor.DarkRed;
-            WriteLine("Input a number (0 or 1 only) now close the application");
-            Environment.Exit(1);
-        }
+        WriteLine(" ------- Let's Make A PBR Resource Pack ------- \nPlease type or copy the full directory of the folder where your base textures are located\nThis is usually the textures/blocks folder in your Minecraft resource pack\n");
 
 
-        // Store all full file directories
+        // Store all file directories
         string _ = ReadLine();
         string[] image_Directories_Full = Directory.GetFiles(_);
 
@@ -47,36 +31,40 @@ internal class TSMaker
 
 
         // Define the folders
-        string folderPath = _ + @"\Jsons\";
-        if (folderPath.EndsWith(@"\\"))
-            folderPath.Replace(@"\\", @"\");
-        Directory.CreateDirectory(folderPath);
+        string folderPath_Heightmap = _ + @"\Heightmap Jsons\";
+        if (folderPath_Heightmap.EndsWith(@"\\"))
+            folderPath_Heightmap.Replace(@"\\", @"\");
+        Directory.CreateDirectory(folderPath_Heightmap);
+
+        string folderPath_Normal = _ + @"\Normal Jsons\";
+        if (folderPath_Normal.EndsWith(@"\\"))
+            folderPath_Normal.Replace(@"\\", @"\");
+        Directory.CreateDirectory(folderPath_Normal);
 
         string pbrMap_FolderPath_MER = _ + @"\TextureSets\MER\";
         if (pbrMap_FolderPath_MER.EndsWith(@"\\"))
             pbrMap_FolderPath_MER.Replace(@"\\", @"\");
         Directory.CreateDirectory(pbrMap_FolderPath_MER);
 
-        string pbrMap_FolderPath_NH = _ + @"\TextureSets\Normal.Heightmap\";
-        if (pbrMap_FolderPath_NH.EndsWith(@"\\"))
-            pbrMap_FolderPath_NH.Replace(@"\\", @"\");
-        Directory.CreateDirectory(pbrMap_FolderPath_NH);
+        string pbrMap_FolderPath_Heightmap = _ + @"\TextureSets\Heightmap\";
+        if (pbrMap_FolderPath_Heightmap.EndsWith(@"\\"))
+            pbrMap_FolderPath_Heightmap.Replace(@"\\", @"\");
+        Directory.CreateDirectory(pbrMap_FolderPath_Heightmap);
 
-
-
+        string pbrMap_FolderPath_Normal = _ + @"\TextureSets\Normal\";
+        if (pbrMap_FolderPath_Normal.EndsWith(@"\\"))
+            pbrMap_FolderPath_Normal.Replace(@"\\", @"\");
+        Directory.CreateDirectory(pbrMap_FolderPath_Normal);
 
 
         // Get rid of unsupported formats
-        for (int i = image_Directories_Full_List.Count() - 1; i >= 0; i--)  // int i is -1 because indexes are zero-based, that means if there are 40 items in our list, we'll need to count from 39 to 0 (which is 40 items in total)
+        for (int i = image_Directories_Full_List.Count() - 1; i >= 0; i--)  
         {
-
             if (!(image_Directories_Full_List[i].EndsWith(".png") || image_Directories_Full_List[i].EndsWith(".jpg") || image_Directories_Full_List[i].EndsWith(".tga") || image_Directories_Full_List[i].EndsWith(".jpeg")))
             {
                 image_Directories_Full_List.RemoveAt(i);
             }
-
         }
-
 
 
         // Factory
@@ -92,64 +80,84 @@ internal class TSMaker
             else if (image_Name_Without_Extension.EndsWith("texture_set")) continue;
             else if (image_Name_Without_Extension.EndsWith("normal") && !(image_Name_Without_Extension.StartsWith("sandstone") || image_Name_Without_Extension.StartsWith("red_sandstone") || image_Name_Without_Extension.StartsWith("rail"))) continue;
 
-            string json_Fullpath = folderPath + image_Name_Without_Extension + ".texture_set.json";
-            WriteLine(json_Fullpath);
 
+            string Normal_json_Fullpath = folderPath_Normal + image_Name_Without_Extension + ".texture_set.json";
+            string Heightmap_json_Fullpath = folderPath_Heightmap + image_Name_Without_Extension + ".texture_set.json";
+            WriteLine(Normal_json_Fullpath + "\n" + Heightmap_json_Fullpath);
             //------------------------------------------- Base information which we use to create the json's content
-            string json_File_Content = "{\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}}";
+            string Normaljson_File_Content = "{\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}}";
+            string Heightmapjson_File_Content = "{\"format_version\":\"1.16.100\",\"minecraft:texture_set\":{\"color\":\"X\",\"metalness_emissive_roughness\":\"Y\",\"W\":\"Z\"}}";
 
             string MER = image_Name_Without_Extension + "_mer";
             string normal = image_Name_Without_Extension + "_normal";
             string heightmap = image_Name_Without_Extension + "_heightmap";
 
-            string[] NH = { "normal", "heightmap", normal, heightmap };
+            // string[] NH = { "normal", "heightmap", normal, heightmap };
             //------------------------------------ Customize this if you have named your textures in a different way, this will be in users hands in the future along with [1]
 
-            json_File_Content = json_File_Content.Replace("X", image_Name_Without_Extension);
-            json_File_Content = json_File_Content.Replace("Y", MER);
-            json_File_Content = json_File_Content.Replace("W", NH[answer]);
-            json_File_Content = json_File_Content.Replace("Z", NH[answer + 2]);
+            Normaljson_File_Content = Normaljson_File_Content.Replace("X", image_Name_Without_Extension);
+            Normaljson_File_Content = Normaljson_File_Content.Replace("Y", MER);
+            Normaljson_File_Content = Normaljson_File_Content.Replace("W", "normal");
+            Normaljson_File_Content = Normaljson_File_Content.Replace("Z", normal);
 
-            File.WriteAllText(json_Fullpath, json_File_Content);
-            // JSON Creation Ends here, variables will be reused.
+            Heightmapjson_File_Content = Heightmapjson_File_Content.Replace("X", image_Name_Without_Extension);
+            Heightmapjson_File_Content = Heightmapjson_File_Content.Replace("Y", MER);
+            Heightmapjson_File_Content = Heightmapjson_File_Content.Replace("W", "heightmap");
+            Heightmapjson_File_Content = Heightmapjson_File_Content.Replace("Z", heightmap);
+
+
+
+            File.WriteAllText(Normal_json_Fullpath, Normaljson_File_Content);
+            File.WriteAllText(Heightmap_json_Fullpath, Heightmapjson_File_Content);
+            // JSON Creation Ends here. variables will be reused.
 
 
 
 
 
-            // PBR Texture files (there will be more operations later, manifest generation and many things will come even later) [1]
+            // PBR Texture files copied to their directories (later processed)
             string image_Extension = Path.GetExtension(listed_image_Directories_Full);
 
             string MER_Destination_File = pbrMap_FolderPath_MER + image_Name_Without_Extension + "_mer" + image_Extension;
             File.Copy(listed_image_Directories_Full, MER_Destination_File, true);
 
-            string NormalHeightmap_Destination_File = pbrMap_FolderPath_NH + image_Name_Without_Extension + "_"+NH[answer] + image_Extension;
-            File.Copy(listed_image_Directories_Full, NormalHeightmap_Destination_File, false);
+            string Normal_Destination_File = pbrMap_FolderPath_Normal + image_Name_Without_Extension + "_normal" + image_Extension;
+            File.Copy(listed_image_Directories_Full, Normal_Destination_File, false);
 
-        }
-
-
-
+            string Heightmap_Destination_File = pbrMap_FolderPath_Heightmap + image_Name_Without_Extension + "_heightmap" + image_Extension;
+            File.Copy(listed_image_Directories_Full, Heightmap_Destination_File, false);
 
 
-        // Finish
-        if (answer == 0 || answer == 1)
-        {
-            WriteLine($"\nSUCCESSFUL! Find JSONS folder at:\n{folderPath}");
+
+
+
+
+
+
+        } //end of foreach
+
+
+
+
+
+
+
+            // Finish
+            WriteLine($"\nSUCCESSFUL!");
             SoundPlayer finishSound = new SoundPlayer("finish.wav");
             finishSound.Load();
             finishSound.Play();
-        }
-        ReadLine();
+            ReadLine();
     }
 }
 
 // ideas/plans:
 // Re-Run the app whenever user types anything wrong, could be a few cases
 // Reading subdirectories and placing the jsons in the directories with the same folder name inside of JSONS folder.
-// e.g get files in /blocks/candles, and places candles jsons in JSONS/candles folders.
+//// e.g get files in /blocks/candles, and places candles jsons in JSONS/candles folders.
 // Move it all to an actual Ui
-
+// No more "Options" user will just input the pack directory, heightmap and normal jsons are both generated in seperate folders
+//// And heightmaps and normals are both generated too, no more asking for 0 and 1, do it all at once. everything.
 
 // v3 so far:
 // Get files and create copies of the them with the same name + _mer/normal/heightmap in the same directory (Optional Feature) [will add  optionlater]
